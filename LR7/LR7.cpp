d586ec8a0e5c181f6f3e4210b8b8eea635199fc5
“John <tttks31122016@gmail.com>
2022-11-28 15:38:04 +0200

    LABA OP

---
+++ LR7/LR7.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std; 
+int main()
+{
+    float y, Sum, x;
+    cout << "input x";
+    cin >> x;
+    y = 0;
+    for (int i = 1; i < 4; i++)
+    {
+        y += i;
+
+        y = log(Sum * i) + (pow(x, 2));
+        cout << 'y=' << y << endl;
+    }
+}
+
